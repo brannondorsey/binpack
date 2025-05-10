73646bd30faa50aef3b5e8f8105ecb3509e25774
Brannon Dorsey <brannon@brannondorsey.com>
2025-05-10 19:25:57 -0400

    chore: Split into main.rs and lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,55 @@ use good_lp::{
 };
 use std::{collections::BTreeMap, fs::read_to_string};
 
-mod types;
-use types::{Input, Output, SoftRequirement, WorkloadSpec};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize)]
+pub struct Input {
+    pub workloads: BTreeMap<String, WorkloadSpec>,
+    pub clusters: BTreeMap<String, u32>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct WorkloadSpec {
+    pub replicas: u32,
+    #[serde(rename = "groupSize")]
+    pub group_size: Option<u32>,
+    pub affinity: Option<Affinity>,
+    #[serde(rename = "antiAffinity")]
+    pub anti_affinity: Option<AntiAffinity>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Affinity {
+    pub soft: Option<Vec<SoftRequirement>>,
+    pub hard: Option<HardRequirement>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct AntiAffinity {
+    pub soft: Option<Vec<SoftRequirement>>,
+    pub hard: Option<HardRequirement>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Output {
+    pub solution: BTreeMap<String, BTreeMap<String, u32>>,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct SoftRequirement {
+    #[serde(default = "default_weight")]
+    pub weight: f64,
+    pub clusters: Vec<String>,
+}
+fn default_weight() -> f64 {
+    1.0
+}
+
+#[derive(Debug, Deserialize, Clone, Default)]
+pub struct HardRequirement {
+    pub clusters: Vec<String>,
+}
 
 // TODO: Manually validate replica counts are multiples of group sizes
 
@@ -22,20 +69,7 @@ use types::{Input, Output, SoftRequirement, WorkloadSpec};
 // non-linear operations, so we must reformulate them using auxiliary variables and linear
 // relationships like we've done here.
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let path = std::env::args()
-        .nth(1)
-        .expect("Usage: <program> <input_file.yaml>");
-
-    let buf = read_to_string(path)?;
-    let input: Input = serde_yaml::from_str(&buf)?;
-    let solution = solve(input)?;
-
-    println!("{}", serde_yaml::to_string(&solution)?);
-    Ok(())
-}
-
-fn solve(input: Input) -> Result<Output, Box<dyn std::error::Error>> {
+pub fn solve(input: Input) -> Result<Output, Box<dyn std::error::Error>> {
     let workloads = &input.workloads;
     let clusters = &input.clusters;
     let init_zero = Expression::from(0.0);
---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::fs::read_to_string;
+
+use binpack::{Input, solve};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let path = std::env::args()
+        .nth(1)
+        .expect("Usage: <program> <input_file.yaml>");
+
+    let buf = read_to_string(path)?;
+    let input: Input = serde_yaml::from_str(&buf)?;
+    let solution = solve(input)?;
+
+    println!("{}", serde_yaml::to_string(&solution)?);
+    Ok(())
+}
